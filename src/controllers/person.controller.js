795418a7c0b89b1347128c61841001f564ce6012
jovenesipup <jovenes.ipup@gmail.com>
2022-09-30 00:04:47 -0500

    sdfsdf

--- src/controllers/person.controller.js
+++ src/controllers/person.controller.js
@@ -30,7 +30,7 @@ export const findOnePerson = async (req, res) => {
 };
 
 export const findOnePersonByDni = async (req, res) => {
-  const person = await Person.findOnePersonByDni(req.params.dni);
+  const person = await Person.find({dni: req.params.dni});
   res.json(person);
 };
 
