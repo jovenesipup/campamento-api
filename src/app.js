ec81db67abcda8b06f0488e626972269aafc3bc8
jovenesipup <jovenes.ipup@gmail.com>
2022-10-06 07:13:19 -0500

    sdsdfdsf

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import express from "express";
 import IndexRoutes from "./routes/index";
 import morgan from "morgan";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
 
 app.set("port", process.env.PORT || 3001);
 
-app.use(cors())
+app.use(cors());
 app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
 
 app.get("/", (req, res) => {
   res.json({ messase: "Welcome" });
